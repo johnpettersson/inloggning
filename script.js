50c20189be768a1462e307668874d6ccf13a071f
John Pettersson <john.pettersson.vgr@gmail.com>
2022-09-28 14:38:37 +0200

    Finished JS-assignment, might polish later :)

--- script.js
+++ script.js
@@ -1,4 +1,81 @@
-
+/**
+// HTML consists of 3 forms, login, logout and a "return to login" form.
+ * 
+ */
 
 const VALID_USERNAME = "Sara";
 const VALID_PASSWORD = "qwe123"
+
+// Below i fetch the forms and add eventhandlers for onsubmit
+const loginForm = document.getElementById("loginform");
+loginForm.addEventListener("submit", onLoginSubmit);
+
+
+const logoutForm = document.getElementById("logoutform");
+logoutForm.addEventListener("submit", onLogoutSubmit);
+
+const invalidloginform = document.getElementById("invalidloginform");
+invalidloginform.addEventListener("submit", returnToLoginForm);
+
+const statusText = document.getElementById("status");
+
+
+function isLoggedIn() {
+    return localStorage.getItem("user") !== null;
+}
+
+if(isLoggedIn()) {
+    statusText.innerText = "Du är inloggad som: " + VALID_USERNAME;
+    swapForms();
+}
+
+//swap visibility between login/logout forms
+function swapForms() {
+    loginForm.classList.toggle("hidden");
+    logoutForm.classList.toggle("hidden");
+}
+
+
+function onLogoutSubmit(e) {
+    //remove user form localStorage and swap back to loginForm
+    localStorage.removeItem("user");
+    swapForms();
+
+    //prevents form from being sent to backend
+    e.preventDefault();
+    return false;
+}
+
+
+function onLoginSubmit(e) {
+    //gets data from the loginform
+    let formData = new FormData(loginForm);
+    let user = formData.get("username");
+    let password = formData.get("password");
+
+    if(user == VALID_USERNAME && password == VALID_PASSWORD) { //save user in localStorage
+        localStorage.setItem("user", VALID_USERNAME);
+        statusText.innerText = "Du är inloggad som: " + VALID_USERNAME;
+        swapForms();
+    } else { // swap to invalidLoginForm
+        loginForm.reset();
+        loginForm.classList.add("hidden");
+        invalidloginform.classList.remove("hidden");
+    }
+
+    //prevents form from being sent to backend
+    e.preventDefault();
+    return false;
+}
+
+function returnToLoginForm(e) {
+    //swap visibility back to loginForm
+    loginForm.classList.remove("hidden");
+    invalidloginform.classList.add("hidden");
+
+    //prevents form from being sent to backend
+    e.preventDefault();
+    return false;
+}
+
+
