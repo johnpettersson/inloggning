bdfb404542e277cd3c274ebe5081b48e3c97a6f3
John Pettersson <john.pettersson.vgr@gmail.com>
2022-10-04 14:33:26 +0200

    removed comments

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-/**
- * @author John Pettersson
-// The HTML consists of 3 forms, login, logout and a "return to login" form.
- * This code handles login/logout and swapping visibility between the forms
- */
 
 const USER_LOCAL_STORAGE_KEY = "User";
 
 const VALID_USERNAME = "Sara";
 const VALID_PASSWORD = "qwe123"
 
-// Below i find the forms and add eventhandlers for onsubmit
 const loginForm = document.getElementById("loginform");
 loginForm.addEventListener("submit", onLoginSubmit);
 
@@ -27,13 +21,11 @@ function isLoggedIn() {
     return localStorage.getItem(USER_LOCAL_STORAGE_KEY) !== null;
 }
 
-// if user is logged in -> swap to logoutform
 if(isLoggedIn()) {
     statusText.innerText = "Du är inloggad som: " + VALID_USERNAME;
     swapForms();
 }
 
-//swap visibility between login/logout forms
 function swapForms() {
     loginForm.classList.toggle("hidden");
     logoutForm.classList.toggle("hidden");
@@ -41,43 +33,37 @@ function swapForms() {
 
 
 function onLogoutSubmit(e) {
-    //remove user form localStorage and swap back to loginForm
     localStorage.removeItem(USER_LOCAL_STORAGE_KEY);
     swapForms();
 
-    //prevents form from being sent to backend
     e.preventDefault();
     return false;
 }
 
 
 function onLoginSubmit(e) {
-    //gets data from the loginform
     let formData = new FormData(loginForm);
     let user = formData.get("username");    
     let password = formData.get("password");
-    loginForm.reset(); //clears data in loginForm
+    loginForm.reset(); 
     
-    if(user == VALID_USERNAME && password == VALID_PASSWORD) { //save user in localStorage
+    if(user == VALID_USERNAME && password == VALID_PASSWORD) { 
         localStorage.setItem(USER_LOCAL_STORAGE_KEY, VALID_USERNAME);
         statusText.innerText = "Du är inloggad som: " + VALID_USERNAME;
         swapForms();
-    } else { // invalid user/pw -> swap to invalidLoginForm
+    } else { 
         loginForm.classList.add("hidden");
         invalidloginform.classList.remove("hidden");
     }
 
-    //prevents form from being sent to backend
     e.preventDefault();
     return false;
 }
 
 function returnToLoginForm(e) {
-    //swap visibility back to loginForm
     loginForm.classList.remove("hidden");
     invalidloginform.classList.add("hidden");
 
-    //prevents form from being sent to backend
     e.preventDefault();
     return false;
 }
